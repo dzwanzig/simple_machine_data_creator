4c17238390c58715a06c4c3b11c59d9e29d46a96
dorianzwanzig <kontakt@dorianzwanzig.de>
2018-11-12 11:16:10 +0100

    little cleanup done :)

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ except:
 # Kopfzeile schreiben
 d.write("ID;Uhrzeit;Drehzahl;Leistungsaufnahme;Vibration;Lautstaerke;Temperatur;FehlerID\n")
 
-# Standardwerte:
+# Startwerte (STartData):
 std_dz = 100             # Standarddrehzahl
 std_la = 18.5            # Standardleistungsaufnahme
 std_vb = 0               # Standardvibration
@@ -26,7 +26,7 @@ std_ls = 75              # Standardlautstärke
 data_id_nr = 10000000    # Startwert für die ID Erstellung
 std_te = 100             # Standardtemperatur
 fehler_id = "leer"       # Fehlerwert bei Ausfall
-starttime = datetime(2018, 1, 1, 0, 0, 0)   # Startzeit der Simulation
+time = datetime(2018, 1, 1, 0, 0, 0)   # Startzeit der Simulation
 
 # Startzeile setzen
 zeile = 1
@@ -34,48 +34,51 @@ zeile = 1
 # Nutzerabfrage: Menge Datensätze
 menge = int(input("Bitte Anzahl der gewünschten Datensätze eingeben: ")) + zeile
 
+# Werte in Ausgabedatei schreiben
+
+
+def write_data():
+    d.write("ABC" + str(data_id_nr) + ";" + str(time) + ";" + str(std_dz) + ";" + str(std_la) +
+            ";" + str(std_vb) + ";" + str(std_ls) + ";" + str(std_te) + ";" + str(fehler_id) + "\n")
+
 # Erstellung mehrerer Datensätze durch Schleife (Normalbetrieb)
 
 
 def normalbetrieb():
-    global zeile, menge, std_dz, std_la, std_vb, std_ls, data_id_nr, std_te, starttime
+    global zeile, menge, std_dz, std_la, std_vb, std_ls, data_id_nr, std_te, time, fehler_id
     x = 0
     y = random.randrange(1, 200)
     while x < y:
-        drehzahl = std_dz
-        leistungsaufnahme = round(np.random.normal(std_la, 0.1), 3)
-        vibration = round(np.random.normal(std_vb, 0.1), 3)
-        lautstaerke = round(np.random.normal(std_ls, 0.1), 3)
-        data_id_nr = 10000000 + zeile
-        time = starttime + timedelta(seconds=30 * zeile)
-        write_data(data_id_nr, time, drehzahl, leistungsaufnahme,
-                   vibration, lautstaerke, temperatur, fehler_id)
+        std_dz = 100
+        std_la = round(np.random.normal(18.5, 0.1), 3)
+        std_vb = round(np.random.normal(0, 0.1), 3)
+        std_ls = round(np.random.normal(75, 0.1), 3)
+        data_id_nr = data_id_nr + 1
+        time = time + timedelta(seconds=30)
+        std_te = 100
+        fehler_id = "0000"
+        write_data()
         zeile = zeile + 1
         x = x + 1
-    return zeile
 
 # Erstellung mehrerer Datensätze durch Schleife (Wartung)
 
 
 def wartung():
-    global zeile, menge, std_dz, std_la, std_vb, std_ls, data_id_nr, std_te, starttime
+    global zeile, menge, std_dz, std_la, std_vb, std_ls, data_id_nr, std_te, time, fehler_id
     fehler_id = wartung_grund()
-    temperatur = std_te
     x = 0
     y = random.randrange(10, 50)
     while x < y:
-        drehzahl = 0
-        leistungsaufnahme = round(np.random.normal(std_la*0.1, 0.1), 3)
-        vibration = round(np.random.normal(std_vb*2, 0.1), 3)
-        lautstaerke = round(np.random.normal(std_ls*0.3, 0.1), 3)
-        data_id_nr = 10000000 + zeile
-        temperatur = temperatur - 2
-        time = starttime + timedelta(seconds=30 * zeile)
-        write_data(data_id_nr, time, drehzahl, leistungsaufnahme,
-                   vibration, lautstaerke, temperatur, fehler_id)
+        std_dz = 0
+        std_la = round(np.random.normal(2, 0.1), 3)
+        std_vb = round(np.random.normal(0, 0.3), 3)
+        std_ls = round(np.random.normal(25, 0.1), 3)
+        time = time + timedelta(seconds=30)
+        std_te = std_te - 1
+        write_data()
         zeile = zeile + 1
         x = x + 1
-    return zeile
 
 # Wartungsgrund auswählen
 
@@ -97,34 +100,32 @@ def wartung_grund():
 
 
 def ausfall_2():
-    global zeile, menge, std_dz, std_la, std_vb, std_ls, data_id_nr, std_te, starttime
-    fehler_id = "F002"
-    temperatur = std_te
+    global zeile, menge, std_dz, std_la, std_vb, std_ls, data_id_nr, std_te, time, fehler_id
+    fehler_id = "0000"
     x = 1
-    # Strombedarf steigt bis zum Ausfall
-    while temperatur < 200:
-        drehzahl = 100 + random.randrange(-2, 2)
-        leistungsaufnahme = round(np.random.normal(std_la*x, 0.2), 3)
-        vibration = round(np.random.normal(std_vb, 0.1), 3)
-        lautstaerke = round(np.random.normal(std_ls, 0.1), 3)
-        data_id_nr = 10000000 + zeile
-        time = starttime + timedelta(seconds=30 * zeile)
-        temperatur = round(temperatur + np.random.normal(2, 0.5), 1)
-        write_data(data_id_nr, time, drehzahl, leistungsaufnahme,
-                   vibration, lautstaerke, temperatur, fehler_id)
+    # Motortemperatur steigt bis zum Ausfall
+    while std_te < 200:
+        std_dz = 100 + random.randrange(-2, 2)
+        std_la = round(np.random.normal(18.5*x, 0.2), 3)
+        std_vb = round(np.random.normal(0, 0.1), 3)
+        std_ls = round(np.random.normal(75, 0.1), 3)
+        data_id_nr = data_id_nr + 1
+        time = time + timedelta(seconds=30)
+        std_te = round(std_te + np.random.normal(1, 0.5), 1)
+        write_data()
         zeile = zeile + 1
         x = x + round(np.random.normal(0.005, 0.002), 3)
-    # zufällige Ausfallzeit
-    while temperatur > std_te:
-        drehzahl = 0
-        leistungsaufnahme = round(np.random.normal(std_la*0.2, 0.1), 3)
-        vibration = round(np.random.normal(std_vb*2, 0.1), 3)
-        lautstaerke = round(np.random.normal(std_ls*0.3, 0.1), 3)
-        data_id_nr = 10000000 + zeile
-        temperatur = round(temperatur - 1, 1)
-        time = starttime + timedelta(seconds=30 * zeile)
-        write_data(data_id_nr, time, drehzahl, leistungsaufnahme,
-                   vibration, lautstaerke, temperatur, fehler_id)
+    fehler_id = "F002"
+    # Abkühlung bis Normaltemperatur bevor Maschine wieder angeschaltet wird
+    while std_te > 100:
+        std_dz = 0
+        std_la = round(np.random.normal(1.5, 0.1), 3)
+        std_vb = round(np.random.normal(0, 0.05), 3)
+        std_ls = round(np.random.normal(25, 0.1), 3)
+        data_id_nr = data_id_nr + 1
+        std_te = round(std_te - 1, 1)
+        time = time + timedelta(seconds=30)
+        write_data()
         zeile = zeile + 1
         x = x + 1
     return zeile
@@ -133,47 +134,37 @@ def ausfall_2():
 
 
 def ausfall_1():
-    global zeile, menge, std_dz, std_la, std_vb, std_ls, data_id_nr, std_te, starttime
-    fehler_id = "F001"
-    temperatur = std_te
+    global zeile, menge, std_dz, std_la, std_vb, std_ls, data_id_nr, std_te, time, fehler_id
+    fehler_id = "0000"
     x = 1
-    leistungsaufnahme = std_la
     # Strombedarf steigt bis zum Ausfall
-    while leistungsaufnahme < 25:
-        drehzahl = 100 + random.randrange(-2, 2)
-        leistungsaufnahme = round(np.random.normal(std_la*x, 0.2), 3)
-        vibration = round(np.random.normal(std_vb, 0.1), 3)
-        lautstaerke = round(np.random.normal(std_ls, 0.1), 3)
-        data_id_nr = 10000000 + zeile
-        time = starttime + timedelta(seconds=30 * zeile)
-        temperatur = round(temperatur + np.random.normal(0.5, 0.2), 1)
-        write_data(data_id_nr, time, drehzahl, leistungsaufnahme,
-                   vibration, lautstaerke, temperatur, fehler_id)
+    while std_la < 25:
+        std_dz = 100 + random.randrange(-2, 2)
+        std_la = round(np.random.normal(18.5*x, 0.2), 3)
+        std_vb = round(np.random.normal(0, 0.1), 3)
+        std_ls = round(np.random.normal(75, 0.1), 3)
+        data_id_nr = data_id_nr + 1
+        time = time + timedelta(seconds=30)
+        std_te = round(std_te + np.random.normal(0.5, 0.2), 1)
+        write_data()
         zeile = zeile + 1
         x = x + round(np.random.normal(0.01, 0.005), 3)
+    fehler_id = "F001"
     # zufällige Ausfallzeit
     x = 0
     y = random.randrange(10, 50)
     while x < y:
-        drehzahl = 0
-        leistungsaufnahme = round(np.random.normal(std_la*0.2, 0.1), 3)
-        vibration = round(np.random.normal(std_vb*2, 0.1), 3)
-        lautstaerke = round(np.random.normal(std_ls*0.3, 0.1), 3)
-        data_id_nr = 10000000 + zeile
-        temperatur = round(temperatur - 1, 1)
-        time = starttime + timedelta(seconds=30 * zeile)
-        write_data(data_id_nr, time, drehzahl, leistungsaufnahme,
-                   vibration, lautstaerke, temperatur, fehler_id)
+        std_dz = 0
+        std_la = round(np.random.normal(1.5, 0.1), 3)
+        std_vb = round(np.random.normal(0, 0.1), 3)
+        std_ls = round(np.random.normal(25, 0.1), 3)
+        data_id_nr = data_id_nr + 1
+        std_te = round(std_te - 1, 1)
+        time = time + timedelta(seconds=30)
+        write_data()
         zeile = zeile + 1
         x = x + 1
     return zeile
-
-# Werte in Ausgabedatei schreiben
-
-
-def write_data(data_id_nr, time, drehzahl, leistungsaufnahme, vibration, lautstaerke, temperatur, fehler_id):
-    d.write("ABC" + str(data_id_nr) + ";" + str(time) + ";" + str(drehzahl) + ";" + str(leistungsaufnahme) +
-            ";" + str(vibration) + ";" + str(lautstaerke) + ";" + str(temperatur) + ";" + str(fehler_id) + "\n")
 
 # Auswahl zwischen den Events
 
