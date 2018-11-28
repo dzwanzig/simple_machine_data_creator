7f9b9235218f96038058f5c6e01a5b8d1e90f1cb
dorianzwanzig <kontakt@dorianzwanzig.de>
2018-11-28 14:24:34 +0100

    einfache plotdarstellung der prediction data

---
+++ test/erg_ana.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pymssql
+import matplotlib.pyplot as plt
+import pandas as pd
+
+password = "Masterprojekt"
+conn = pymssql.connect("pcs.f4.htw-berlin.de",
+                       "Masterprojekt", password, "PraediktiveAnalysenTest")
+cursor = conn.cursor()
+
+df = pd.read_sql('SELECT * FROM predictions', conn)
+
+plt.scatter(df["Timestamp"], df["Ausfallzeitpunkt"])
+# plt.plot([0, 120], predicted, color="red")
+plt.show()
